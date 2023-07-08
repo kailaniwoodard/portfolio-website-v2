f224cd73b327367b560c5733972224f9d9830cd6
Kai'lani Woodard <woodardk@allegheny.edu>
2023-07-08 12:38:57 -0400

    Imported Main component

--- website-nextjs/pages/index.js
+++ website-nextjs/pages/index.js
@@ -1,18 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Navbar from 'components/Navbar'
+import Head from "next/head"
+import Image from "next/image"
+import { Inter } from "next/font/google"
+import Navbar from "components/Navbar"
+import Main from "components/Main"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
   return (
     <div>
       <Head>
-      <title>Kai'lani Woodard</title>
+      <title>Kai"lani Woodard</title>
     </Head>
   <Navbar />
-
+  <Main />
   </div>
   )
 }
