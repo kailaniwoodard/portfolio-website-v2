aaf51478e1893173ffa9dee48459f8730caf5512
Kai'lani Woodard <woodardk@allegheny.edu>
2023-07-07 22:56:45 -0400

    Imported Navbar

---
+++ website-nextjs/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Navbar from 'components/Navbar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+      <title>Kai'lani Woodard</title>
+    </Head>
+  <Navbar />
+
+  </div>
+  )
+}
