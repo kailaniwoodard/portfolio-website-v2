9564097bdfb4d77f31f852cebbf669a4ad1d27d3
Kai'lani Woodard <woodardk@allegheny.edu>
2023-07-08 12:14:26 -0400

    Fixed formatting for Navbar elements

--- website-nextjs/components/Navbar.js
+++ website-nextjs/components/Navbar.js
@@ -24,21 +24,27 @@ const Navbar = () => {
         <div>
           <ul className="hidden md:flex">
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+              <li className="ml-10 text-sm uppercase hover:border-0">
+                Home
+                </li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+              <li className="ml-10 text-sm uppercase hover:border-0">
+                About
+                </li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+              <li className="ml-10 text-sm uppercase hover:border-0">
+                Skills
+                </li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">
+              <li className="ml-10 text-sm uppercase hover:border-0">
                 Projects
               </li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">
+              <li className="ml-10 text-sm uppercase hover:border-0">
                 Contact
               </li>
             </Link>
